9e5cde4bc4d9ab85e81d351f911291e2b80d818f
gokylx <gokylx@gmail.com>
2016-05-18 18:48:40 +0800

    learn innerclass and factory pattern

---
+++ src/innerclasses/Parcel1.java
@@ -0,0 +1,33 @@
+// Createing inner class.
+
+package innerclasses;
+
+public class Parcel1 {
+	class Contents{
+		private int i = 11;
+		public int value() {
+			return i;
+		}
+	}
+	
+	class Destination {
+		private String label;
+		public Destination(String whereTo) {
+			label = whereTo;
+		}
+		String readLabel() { return label; }
+		
+	}
+	
+	// Using inner classes looks just like
+	// using any other class, within Parcel1;
+	
+	public void ship(String dest) {
+		Destination destination = new Destination(dest);
+		System.out.println(destination.readLabel());
+	}
+	public static void main(String[] args) {
+		Parcel1 p = new Parcel1();
+		p.ship("Tasmania");
+	}
+}
---
+++ src/innerclasses/Parcel2.java
@@ -0,0 +1,34 @@
+// Returning a reference to an inner class.
+
+package innerclasses;
+
+public class Parcel2 {
+	class Contents{
+		private int i = 11;
+		public int value() {
+			return i;
+		}
+	}
+	
+	class Destination {
+		private String label;
+		public Destination(String whereTo) {
+			label = whereTo;
+		}
+		String readLabel() { return label; }
+		
+	}
+	
+	public Contents contents() {
+		return new Contents();
+	}
+	
+	public void ship(String dest) {
+		Destination destination = new Destination(dest);
+		System.out.println(destination.readLabel());
+	}
+	public static void main(String[] args) {
+		Parcel2 p = new Parcel2();
+		p.ship("Tasmania");
+	}
+}
