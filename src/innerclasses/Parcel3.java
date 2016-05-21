7b99b72ae5ce67ad0819687006321c87acf7aab3
Gokylx <gokylx@gmail.com>
2016-05-21 23:38:20 +0800

    use .new

---
+++ src/innerclasses/Parcel3.java
@@ -0,0 +1,32 @@
+package innerclasses;
+
+// Using .new to create instance of inner classes.
+public class Parcel3 {
+	class Contents {
+		private int i = 11;
+
+		public int value() {
+			return i;
+		}
+
+	}
+
+	class Destination {
+		private String label;
+
+		Destination(String whereTo) {
+			whereTo = label;
+		}
+
+		String readLabel() {
+			return label;
+		}
+	}
+
+	public static void main(String[] args) {
+		Parcel3 parcel3 = new Parcel3();
+		Contents contents = parcel3.new Contents();
+		Destination destination = parcel3.new Destination("Tasmania");
+	}
+
+}
