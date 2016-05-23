3ea4f7928868fc68f1d25d2f145a69080c20c8ee
gokylx <gokylx@gmail.com>
2016-05-23 18:42:19 +0800

    add dotThis

---
+++ src/innerclasses/DotThis.java
@@ -0,0 +1,23 @@
+// Qualifying access to the outer-class object.
+
+package innerclasses;
+
+public class DotThis {
+	void f() {
+		System.out.println("DotThis.f");
+	}
+	public class Inner {
+		public DotThis outer(){
+			return DotThis.this;
+		}
+	}
+	public Inner inner() {
+		return new Inner();
+	}
+	
+	public static void main(String[] args) {
+		DotThis dotThis = new DotThis();
+		Inner inner = dotThis.inner();
+		inner.outer().f();
+	}
+}
