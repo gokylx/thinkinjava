4c1616d18f3134f2e4d93a77443421fc690f3628
gokylx <gokylx@gmail.com>
2016-05-24 11:54:01 +0800

    add calling the base-class constructor

---
+++ src/innerclasses/Parcel8.java
@@ -0,0 +1,20 @@
+package innerclasses;
+// Calling the base-class constructor.
+public class Parcel8 {
+	public Wrapping wrapping(int x) {
+		return new Wrapping(x) { // Pass constructor argument.
+
+			@Override
+			public int value() {
+				return super.value() * 47;
+			}
+			
+		};
+	}
+	public static void main(String[] args) {
+		Parcel8 parcel8 = new Parcel8();
+		Wrapping wrapping = parcel8.wrapping(10);
+		System.out.println(wrapping.value());
+		
+	}
+}
