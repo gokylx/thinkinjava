962c252e5c056bbab0bd96554a67ff43fb1fec57
gokylx <gokylx@gmail.com>
2016-05-24 08:48:04 +0800

    add a anonymous inner class

---
+++ src/innerclasses/Parcel7.java
@@ -0,0 +1,20 @@
+package innerclasses;
+// Returning an instance of anonymous inner class.
+
+public class Parcel7 {
+	public Contents contents() { // Insert a class definition
+		return new Contents() {
+			private int i = 11;
+			@Override
+			public int value() {
+				return i;
+			}
+		}; // semicolon required in this case
+	}
+	public static void main(String[] args) {
+		Parcel7 parcel7 = new Parcel7();
+		Contents contents = parcel7.contents();
+		int value = contents.value();
+		System.out.println(value);
+	}
+}
