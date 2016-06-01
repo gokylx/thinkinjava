e248276453e4735de0be5f93bfcca500aa11cdd8
gokylx <gokylx@gmail.com>
2016-06-01 19:10:14 +0800

    add collection foreach

---
+++ src/holding/ForeachCollection.java
@@ -0,0 +1,17 @@
+// All collections work with foreach
+package holding;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class ForeachCollection {
+	public static void main(String[] args) {
+		Collection<String> list = new LinkedList<String>();
+		Collections.addAll(list, "long long a way to travel".split(" "));
+
+		for (String string : list) {
+			System.out.print("'" + string + "'");
+		}
+	}
+}
