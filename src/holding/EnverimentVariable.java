567af2f824de13a822fa121cfdc07f3ff6c7e36c
Gokylx <gokylx@gmail.com>
2016-06-01 22:58:10 +0800

    review

--- src/holding/EnverimentVariable.java
+++ src/holding/EnverimentVariable.java
@@ -6,6 +6,8 @@ public class EnverimentVariable {
 	public static void main(String[] args) {
 		for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
 			System.out.println(entry.getKey() + " : " + entry.getValue());
+
 		}
+		// System.out.println(System.getenv().entrySet());
 	}
 }
