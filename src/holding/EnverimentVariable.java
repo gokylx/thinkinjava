d522190567a5f3e8f602e554e8b0af2b1e2c4f6b
Gokylx <gokylx@gmail.com>
2016-06-01 22:14:08 +0800

    add EnverimentVariable.java to print system enverimentvariable

---
+++ src/holding/EnverimentVariable.java
@@ -0,0 +1,11 @@
+package holding;
+
+import java.util.Map;
+
+public class EnverimentVariable {
+	public static void main(String[] args) {
+		for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+	}
+}
