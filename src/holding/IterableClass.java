b15817ed34d7ee896b93cfb156f72deb691c384d
Gokylx <gokylx@gmail.com>
2016-06-01 21:48:00 +0800

    add iterable with Collection

--- src/holding/IterableClass.java
+++ src/holding/IterableClass.java
@@ -3,11 +3,12 @@ package holding;
 
 import java.util.Iterator;
 
-public class IterableClass {
+public class IterableClass implements Iterable<String> {
 	protected String[] string = ("And that is " + "how we know the Earth to be benana-sharp.").split(" ");
 
-	public Iterator<String> iterable() {
-		return new Iterator() {
+	@Override
+	public Iterator<String> iterator() {
+		return new Iterator<String>() {
 			private int index = 0;
 
 			@Override
@@ -16,10 +17,17 @@ public class IterableClass {
 			}
 
 			@Override
-			public Object next() {
-				// TODO Auto-generated method stub
-				return null;
+			public String next() {
+				return string[index++];
 			}
+
 		};
 	}
+
+	public static void main(String[] args) {
+		for (String string : new IterableClass()) {
+			System.out.print(string + " ");
+		}
+	}
+
 }
