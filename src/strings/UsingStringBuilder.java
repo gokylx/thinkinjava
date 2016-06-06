45bef8d300ec6a8609556053bc306d9965adcd58
gokylx <gokylx@gmail.com>
2016-06-06 18:36:00 +0800

    use StringBuilder

---
+++ src/strings/UsingStringBuilder.java
@@ -0,0 +1,32 @@
+package strings;
+
+import java.util.Random;
+
+public class UsingStringBuilder {
+	public static Random rand = new Random(47);
+
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder("[");
+		for (int i = 1; i < 10; i++) {
+			stringBuilder.append(rand.nextInt(100));
+			stringBuilder.append(", ");
+		}
+
+		stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+
+		// stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+		// stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+
+		stringBuilder.append("]");
+		return stringBuilder.toString();
+	}
+
+	public static void main(String[] args) {
+
+		UsingStringBuilder usingStringBuilder = new UsingStringBuilder();
+		System.out.println(usingStringBuilder);
+
+	}
+
+}
