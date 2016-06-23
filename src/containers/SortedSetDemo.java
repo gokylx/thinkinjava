05567a3cc857be51246666a20139f3fe09f0e009
gokylx <gokylx@gmail.com>
2016-06-23 10:45:31 +0800

    add SortedSet class

---
+++ src/containers/SortedSetDemo.java
@@ -0,0 +1,24 @@
+package containers;
+
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class SortedSetDemo {
+	public static void main(String[] args) {
+		SortedSet<String> sort = new TreeSet<>();
+		Collections.addAll(sort, "one two three four five six seven eight nine".split(" "));
+		String head = sort.first();
+		String last = sort.last();
+		System.out.println("sort : " + sort);
+
+		System.out.println("head : " + head);
+		System.out.println("last : " + last);
+
+		System.out.println(sort.headSet("four"));
+		System.out.println(sort.subSet("one", "two"));
+		// System.out.println(sort.subSet("two", "one"));
+		System.out.println(sort.tailSet("six"));
+
+	}
+}
