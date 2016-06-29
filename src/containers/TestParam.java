7a32d6f79b7bf6484c635179e9a90bbf673039b2
gokylx <gokylx@gmail.com>
2016-06-29 10:28:32 +0800

    add reader

---
+++ src/containers/TestParam.java
@@ -0,0 +1,13 @@
+package containers;
+
+public class TestParam {
+	public final int size;
+	public final int loops;
+
+	public TestParam(int size, int loops) {
+		super();
+		this.size = size;
+		this.loops = loops;
+	}
+
+}
