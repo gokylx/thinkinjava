f5b8a885f5e2f50beaed07e3a9d56857d75cbcf5
gokylx <gokylx@gmail.com>
2016-07-05 13:58:27 +0800

    add  Serializable

--- src/io/LargeMappedFiles.java
+++ src/io/LargeMappedFiles.java
@@ -9,13 +9,14 @@ public class LargeMappedFiles {
 	static int length = 0x8FFFFFF;
 
 	public static void main(String[] args) throws Exception {
+		@SuppressWarnings("resource")
 		MappedByteBuffer out = new RandomAccessFile("C:/Users/bytte/workspace/thinkinjava/src/io/test.dat", "rw")
 				.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, length);
 		for (int i = 0; i < length; i++) {
 			out.put((byte) 'x');
 			System.out.println("Finished writing");
 		}
-		for (int i = length / 2; i < length / 2 + 6; i++) {
+		for (int i = length / 2; i < ((length / 2) + 6); i++) {
 			System.out.print((char) out.get(i));
 		}
 
