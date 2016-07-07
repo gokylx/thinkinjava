3cb428566d16cd6cf080e610377ccbecf360245a
TerryGo <terrygox@gmail.com>
2016-07-07 09:02:12 +0800

    add suppresswarnings

--- src/concurrency/Joining.java
+++ src/concurrency/Joining.java
@@ -47,6 +47,7 @@ class Joiner extends Thread {
 public class Joining {
 	public static void main(String[] args) {
 		Sleeper sleepy = new Sleeper("Sleepy", 1500), grumpy = new Sleeper("Grumpy", 1500);
+		@SuppressWarnings("unused")
 		Joiner dopey = new Joiner("Dopey", sleepy), doc = new Joiner("Doc", grumpy);
 		grumpy.interrupt();
 	}
