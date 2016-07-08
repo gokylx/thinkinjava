2c3198be3bddc105f7718665463822fcaa6d3b8a
Terry Go <terrygox@gmail.com>
2016-07-08 17:57:22 +0800

    add lock

---
+++ src/concurrency/AttemptLocking.java
@@ -0,0 +1,43 @@
+package concurrency;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AttemptLocking {
+	private ReentrantLock lock = new ReentrantLock();
+
+	private void untime() {
+		boolean captured = lock.tryLock();
+		try {
+			System.out.println("tryLock(): " + captured);
+		} finally {
+			if (captured) {
+				lock.unlock();
+			}
+		}
+
+	}
+
+	public void timed() {
+		boolean captured = false;
+		;
+		try {
+			captured = lock.tryLock(2, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		try {
+			System.out.println("tryLock(2, TimeUnit.Seconds: " + captured);
+		} finally {
+			if (captured) {
+				lock.unlock();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		final AttemptLocking al = new AttemptLocking();
+		al.untime();
+		al.timed();
+	}
+}
