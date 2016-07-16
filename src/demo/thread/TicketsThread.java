d205d180b14dd9c05eb6fd57e0d6a8bb48faac56
Terry Go <terrygox@gmail.com>
2016-07-16 12:33:58 +0800

    review demo.thread

--- src/demo/thread/TicketsThread.java
+++ src/demo/thread/TicketsThread.java
@@ -12,10 +12,18 @@ class MyThread extends Thread{
 	public void run() {
 		while(ticktsCont > 0){
 			ticktsCont--;
-			System.out.println(name + " 卖出一张票");
+			System.out.println(name + " 卖了1张票,剩余票数为："+ ticktsCont);
 		}
 	}
 }
 public class TicketsThread {
-
+	public static void main(String[] args) {
+		MyThread mt1 = new MyThread("窗口1");
+		MyThread mt2 = new MyThread("窗口2");
+		MyThread mt3 = new MyThread("窗口3");
+		
+		mt1.start();
+		mt2.start();
+		mt3.start();
+	}
 }
